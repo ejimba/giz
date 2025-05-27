1ced5114d48ba28b7df1a274861623bc54410613
Eric Ejimba <ejimba@gmail.com>
2025-05-27 03:41:46 +0300

    remove debug

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\DebugConversationController;
 use App\Http\Controllers\TwilioWebhookController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,10 +12,3 @@ Route::any('/login', function () {
 
 Route::post('/webhooks/twilio/incoming', [TwilioWebhookController::class, 'handleIncomingMessage']);
 Route::post('/webhooks/twilio/status', [TwilioWebhookController::class, 'handleStatusCallback']);
-
-Route::prefix('debug/conversation')->group(function () {
-    Route::get('state', [DebugConversationController::class, 'showState']);
-    Route::post('update-step', [DebugConversationController::class, 'updateStep']);
-    Route::post('reset', [DebugConversationController::class, 'resetConversation']);
-    Route::post('test', [DebugConversationController::class, 'testMessage']);
-});
